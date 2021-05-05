0cfa7d90a67707230ea73875108f714fa335f67c
stefanocalzolari98@gmail.com <stefanocalzolari98@gmail.com>
2021-05-05 18:14:54 +0200

    - CC2D updated - PhysicsMat updated - WindGen modified

--- Assets/Scripts/CC2D.cs
+++ Assets/Scripts/CC2D.cs
@@ -6,7 +6,7 @@ public class CC2D : MonoBehaviour
 {
     private Rigidbody rBody;
 
-    private float t = 0f;
+    //private float t = 0f;
     private float oldInputX = 0f;
     private float inputX;
     //private float inputY
@@ -19,18 +19,24 @@ public class CC2D : MonoBehaviour
     public GameObject groundCheck;
     public LayerMask groundMask;
 
+    private Vector3 move;
+
     private float maxJumpTime;
     private float elapsedJumpTime;
     private float jumpVelocity;
     private bool isGrounded;
-    public bool isJumping {get; private set;}
+    public bool isJumping;
+    private bool isOpposingDirection;
     private Vector3 windForce;
+    private Vector3 totalWindForce;
     private float windForceJumpReduction;
 
     // Start is called before the first frame update
     void Start()
     {
         windForce = Vector3.zero;
+        totalWindForce = Vector3.zero;
+        move = Vector3.zero;
 
         isGrounded = false;
 
@@ -66,9 +72,10 @@ public class CC2D : MonoBehaviour
 
         //Gestione rotazione
         
+        
         if (inputX < 0f)
         { //quando cambia direzione
-            t = 0f;
+            //t = 0f;
             //initialRot = this.transform.rotation;
             //finalRot = Quaternion.LookRotation(Vector3.left, Vector3.up) * this.transform.rotation;
             this.transform.forward = Vector3.left;
@@ -78,11 +85,12 @@ public class CC2D : MonoBehaviour
             this.transform.forward = Vector3.right;
         }
 
-        if (t < 1f)
-        {
+        //if (t < 1f)
+        //{
             //this.transform.rotation = Quaternion.Lerp(initialRot, finalRot, t);
-            t += Time.deltaTime / 500;
-        }
+            //t += Time.deltaTime / 500;
+        //}
+        
 
         oldInputX = inputX;
 
@@ -91,14 +99,7 @@ public class CC2D : MonoBehaviour
     private void FixedUpdate()
     {
         //spostamento orizzontale
-
-        Vector3 move = new Vector3(inputX,0,0);
-        
-        //controllo di non aver raggiunto vel massima
-        //rBody.AddForce(move * moveSpeed, ForceMode.VelocityChange);
-        //rBody.AddForce(move * moveSpeed, ForceMode.Impulse);
-
-        //rBody.velocity = new Vector3(Mathf.Clamp(rBody.velocity.x, -maxVelocity, maxVelocity), rBody.velocity.y, rBody.velocity.z);
+        move.x = inputX;
 
         //jump
 
@@ -110,46 +111,46 @@ public class CC2D : MonoBehaviour
         {
             elapsedJumpTime += Time.fixedDeltaTime;
             Vector3 jumpMove = new Vector3(0, jumpVelocity, 0);
-            //rBody.velocity = new Vector3(rBody.velocity.x, 0f, rBody.velocity.z);
-            rBody.AddForce(new Vector3(0f,-rBody.velocity.y,0f),ForceMode.VelocityChange);
-            rBody.AddForce(-Physics.gravity,ForceMode.Acceleration);
-            rBody.AddForce(jumpMove,ForceMode.VelocityChange);
+            rBody.AddForce(new Vector3(0f, -rBody.velocity.y, 0f), ForceMode.VelocityChange);
+            rBody.AddForce(-Physics.gravity, ForceMode.Acceleration);
+            rBody.AddForce(jumpMove, ForceMode.VelocityChange);
         }
 
-
         //GROUND CHECK
-        if (Physics.CheckSphere(groundCheck.transform.position, 0.1f, groundMask) && !isJumping)
+        else if (Physics.CheckSphere(groundCheck.transform.position, 0.1f, groundMask))
         {
             isGrounded = true;
             //rBody.drag = groundDrag;
         }
+        else
+            isGrounded = false;
 
         //Velocity CAP
+        /*
         if (rBody.velocity.x > maxVelocity)
             rBody.velocity = new Vector3(maxVelocity, rBody.velocity.y, rBody.velocity.z);
         else if (rBody.velocity.x < -maxVelocity)
             rBody.velocity = new Vector3(-maxVelocity, rBody.velocity.y, rBody.velocity.z);
-
-        /*
-        if (!rBody.GetComponent<CC2D>().isJumping)
-            rBody.AddForce(this.transform.forward * windForce, forceMode);
-        //rBody.MovePosition(rBody.transform.position + this.transform.forward * windForce * Time.fixedDeltaTime);
-        else
-            rBody.AddForce(this.transform.forward * windForce / forceJumpReduction, forceMode);
         */
 
         oldInputX = inputX;
 
         //calcolo forza vento applicata
-        Vector3 totalWindForce = Vector3.zero;
         if (!isJumping)
             totalWindForce = windForce;
         else
             totalWindForce = windForce / windForceJumpReduction;
 
+        if (move.x * rBody.velocity.x < 0f) 
+            isOpposingDirection = true;
+        else
+            isOpposingDirection = false;
+
         //applico movimento orizzontale finale
-        if ( (rBody.velocity.x < maxVelocity) && (rBody.velocity.x > -maxVelocity)) //CAP di velocità
-            rBody.AddForce(move * moveSpeed + totalWindForce,ForceMode.Acceleration);
+        if ((rBody.velocity.x < maxVelocity) && (rBody.velocity.x > -maxVelocity) || isOpposingDirection){ //CAP di velocità
+            Vector3 totalMove = move * moveSpeed + totalWindForce;
+            rBody.AddForce(totalMove, ForceMode.Acceleration);
+        }
     }
 
     public void AddWindForce(Vector3 force)
--- Assets/Scripts/WindGenerator.cs
+++ Assets/Scripts/WindGenerator.cs
@@ -7,17 +7,19 @@ public class WindGenerator : MonoBehaviour
 
     public float windForce = 5;
     private CC2D controller;
+    private bool toAddForwardForce, toAddBackwardForce;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GameObject.FindGameObjectWithTag("Player").GetComponent<CC2D>();
+        toAddBackwardForce = toAddForwardForce = false;
     }
 
     private void OnTriggerEnter(Collider col)
     {
-        if (col.CompareTag("Player"))
-            controller.AddWindForce(windForce * this.transform.forward);
+        if (col.gameObject == controller.gameObject)
+            toAddForwardForce = true;
 
         //DEBUG
         Debug.Log("Wind colpisce");
@@ -25,10 +27,24 @@ public class WindGenerator : MonoBehaviour
     
     private void OnTriggerExit(Collider col)
     {
-        if (col.CompareTag("Player"))
-            controller.AddWindForce(-windForce * this.transform.forward);
+        if (col.gameObject == controller.gameObject)
+            toAddBackwardForce = true;
 
         //DEBUG
         Debug.Log("Wind non colpisce");
-    } 
+    }
+
+    private void FixedUpdate()
+    {
+        if (toAddForwardForce)
+        {
+            controller.AddWindForce(windForce * this.transform.forward);
+            toAddForwardForce = false;
+        }
+        if (toAddBackwardForce)
+        {
+            controller.AddWindForce(-windForce * this.transform.forward);
+            toAddBackwardForce = false;
+        }
+    }
 }
