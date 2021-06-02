d339f87dcce4ccf0718f686e79a864e484f6b0f1
LucaDonalisio <donalisioluca11@gmail.com>
2021-06-02 17:08:04 +0200

    section 4 and 5 of platform added

---
+++ Assets/Scripts/Mechanics/FallingPlat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlat : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Invoke("DropPlat", 0.5f);
+            Destroy(gameObject, 2f);
+        }
+    }
+    void DropPlat()
+    {
+        rb.isKinematic = false;
+    }
+}
