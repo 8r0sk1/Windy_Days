4e1bd2a1c7658feb4a6fdef6136d96d3464fc4c2
stefanocalzolari98@gmail.com <stefanocalzolari98@gmail.com>
2021-07-02 18:28:39 +0200

    - New scenes - Warper between scenes added

---
+++ Assets/TriggerNewScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerNewScene : MonoBehaviour
+{
+
+    public int sceneIndex;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //DEBUG
+            Debug.Log("new scene loading: "+ sceneIndex);
+            SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+        }
+    }
+}
