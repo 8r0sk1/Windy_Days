671aa2d57dffe548018c46c270e4ca62d0e21a21
LucaDonalisio <donalisioluca11@gmail.com>
2021-07-06 11:41:40 +0200

    dialogue manager added

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+	public Text nameText;
+	public Text dialogueText;
+	public GameObject panel;
+	//public Animator animator;
+
+	private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start()
+	{
+		sentences = new Queue<string>();
+	}
+
+	public void StartDialogue(Dialogue dialogue)
+	{
+		//animator.SetBool("IsOpen", true);
+
+		nameText.text = dialogue.name;
+		panel.SetActive(true);
+		sentences.Clear();
+
+		foreach (string sentence in dialogue.sentences)
+		{
+			sentences.Enqueue(sentence);
+		}
+		Debug.Log(sentences.Count);
+		DisplayNextSentence();
+
+	}
+
+	public void DisplayNextSentence()
+	{
+		Debug.Log(sentences.Count);
+		if (sentences.Count == 0) 
+		{
+			EndDialogue();
+			return;
+		}
+
+		string sentence = sentences.Dequeue();
+		StopAllCoroutines();
+		StartCoroutine(TypeSentence(sentence));
+	}
+
+	IEnumerator TypeSentence(string sentence)
+	{
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return null;
+		}
+	}
+
+	void EndDialogue()
+	{
+		//animator.SetBool("IsOpen", false);
+		panel.SetActive(false);
+	}
+
+}
+
+
---
+++ Assets/Scripts/StartDialogue.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartDialogue : MonoBehaviour
+{
+    private GameObject player;
+    private PlayerManager playerManager;
+    public DialogueTrigger trigger;
+    public DialogueManager dialogueManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerManager = player.GetComponent<PlayerManager>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            trigger.TriggerDialogue();
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == player && Input.GetButtonDown("Use"))
+        {
+            Debug.Log("next sentence");
+            dialogueManager.DisplayNextSentence();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
