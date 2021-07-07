0ff6b6ca5a093cfbc8684e0aa45d32fbf3393c28
stefanocalzolari98@gmail.com <stefanocalzolari98@gmail.com>
2021-07-07 13:01:53 +0200

    - FSM troll and robot updated (animation exit time disabled) - Parry mechanic added

---
+++ Assets/Parry.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parry : MonoBehaviour
+{
+    public float invincibleTime = 0.5f;
+    private PlayerManager playerManager;
+
+    private void Start()
+    {
+        playerManager = GetComponentInParent<PlayerManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("EnemyWeapon"))
+        {
+            playerManager.SetInvincibleTimer(invincibleTime);
+        }
+    }
+}
