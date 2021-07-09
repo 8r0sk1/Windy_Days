44b5ad2d2340eb4337a04b11ba4dd7f4f004c16c
Alberto Roletto <alberto.roletto@gmail.com>
2021-07-09 17:40:18 +0200

    Troll Lock update

---
+++ Assets/Scripts/WarperLock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarperLock : MonoBehaviour
+{
+    public GameObject warper;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Lock()
+    {
+        warper.SetActive(false);
+    }
+   public void Unlock()
+    {
+        warper.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        Unlock();
+    }
+}
