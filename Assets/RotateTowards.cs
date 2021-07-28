2f48c21b3fa45e23c001b0b633be4af837d39846
stefanocalzolari98@gmail.com <stefanocalzolari98@gmail.com>
2021-07-28 17:01:43 +0200

    - UI buttons added (TO FIX)

---
+++ Assets/RotateTowards.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowards : MonoBehaviour
+{
+
+    private GameObject cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = cam.transform.position - this.transform.position;
+        this.transform.forward = dir.normalized;
+    }
+}
--- Assets/TriggerNewScene.cs
+++ Assets/TriggerNewScene.cs
@@ -10,11 +10,21 @@ public class TriggerNewScene : MonoBehaviour
     public int nextEntryPoint;
     public bool isBackground;
     private Animator Fade_anim;
+    private GameObject UI_stickUp;
+
+    private void Start()
+    {
+        //UI_stickUp = GameObject.FindGameObjectWithTag("UI_stickUp");
+        //UI_stickUp.SetActive(false);
+    }
 
     private void Awake()
     {
         Fade_anim = GameObject.FindGameObjectWithTag("Fade").GetComponent<Animator>();
+        UI_stickUp = GameObject.FindGameObjectWithTag("UI_stickUp");
+        UI_stickUp.SetActive(false);
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (!isBackground)
@@ -27,6 +37,11 @@ public class TriggerNewScene : MonoBehaviour
                 FadeOut_state.SetSceneIndex(sceneIndex);
                 Fade_anim.SetTrigger("_fadeOUT");
             }
+        }
+        //UI STICK UP
+        else
+        {
+            UI_stickUp.SetActive(true);
         }
     }
 
@@ -42,6 +57,14 @@ public class TriggerNewScene : MonoBehaviour
                 FadeOut_state.SetSceneIndex(sceneIndex);
                 Fade_anim.SetTrigger("_fadeOUT"); 
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (isBackground)
+        {
+            UI_stickUp.SetActive(false);
         }
     }
 }
