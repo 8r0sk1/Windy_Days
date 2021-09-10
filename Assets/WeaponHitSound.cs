b757ee36e3ec1f104204442c5399b9b191e836ff
stefanocalzolari98@gmail.com <stefanocalzolari98@gmail.com>
2021-09-10 16:31:16 +0200

    - Robot HitSound fixed - Invisible wall (TD2) added

--- Assets/WeaponHitSound.cs
+++ Assets/WeaponHitSound.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WeaponHitSound : MonoBehaviour
 {
+    public AudioClip trollHitAudio, robotHitAudio;
     public AudioSource source;
     public GameObject bloodParticles;
     public GameObject sparkParticles;
@@ -23,13 +24,11 @@ public class WeaponHitSound : MonoBehaviour
     private void OnTriggerEnter(Collider col)
     {
 
-        if (col.CompareTag("Enemy"))
-        {
-            source.Play();
-        }
-
         if (col.gameObject.GetComponent<CustomTag>().HasTag("Troll"))
         {
+            source.clip = trollHitAudio;
+            //source.pitch = 1f;
+
             GameObject newParticles = Instantiate(bloodParticles, null);
             newParticles.transform.position = col.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
             //newParticles.transform.forward = col.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
@@ -37,12 +36,20 @@ public class WeaponHitSound : MonoBehaviour
         
         if (col.gameObject.GetComponent<CustomTag>().HasTag("Robot_A") || col.gameObject.GetComponent<CustomTag>().HasTag("Robot_B"))
         {
+            source.clip = robotHitAudio;
+            //source.pitch = 0.8f;
+
             //DEBUG
             Debug.Log("SMAZZOLANDO IL ROBOT");
 
             GameObject newParticles = Instantiate(sparkParticles, null);
             newParticles.transform.position = col.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
             //newParticles.transform.forward = col.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
+        }
+
+        if (col.CompareTag("Enemy"))
+        {
+            source.Play();
         }
     }
 }
